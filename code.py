2421983c87d0990a3681664318eec8e51c163e22
Ganeshan Jayaraman <56891191+ganeshan-blink@users.noreply.github.com>
2021-08-05 11:26:29 -0700

    Initial Commit

--- code.py
+++ code.py
@@ -1,12 +1,97 @@
-class Test:
-  
-  def main(a: str):
-    print(a)
-  
-  
-def main():
-    localTest = Test()
-    localTest.main("Hello World!")
+# AC: Return the lowest priced medication in dollars given a list of medication attributes subject to:
+# Matching drug class: All medications with the same drug class number are equivalent
+# Prefer generics over brand on equivalent price
+# Don't dispense medication if max quantity is exceeed  
 
-if __name__ == "__main__":
-    main()
+medications = [
+    {
+       'name': 'losartan',
+       'type': 'generic',
+       'class_number': 1,
+       'price_cents': 1400,
+       'currency': 'pound',
+       'unit_type': 'tablet',
+       'max_quantity': 90
+
+    },
+    {
+       'name': 'cozaar',
+       'type': 'brand',
+       'class_number': 1,
+       'price_cents': 3400,
+       'currency': 'dollar',
+       'unit_type': 'tablet',
+       'max_quantity': 25
+
+    },
+    {
+       'name': 'Glimepiride',
+       'type': 'generic',
+       'class_number': 2,
+       'price_cents': 2400,
+       'currency': 'dollar',
+       'unit_type': 'tablet',
+       'max_quantity':15
+    },
+    {
+       'name': 'Amaryl',
+       'type': 'brand',
+       'class_number': 2,
+       'price_cents': 2400,
+       'currency': 'dollar',
+       'unit_type': 'tablet',
+       'max_quantity': 15
+
+    },
+    {
+       'name': 'Glucotrol',
+       'type': 'brand',
+       'class_number': 2,
+       'price_cents': 3000,
+       'currency': 'euro',
+       'unit_type': 'tablet',
+       'max_quantity': 10
+    }
+    ,
+    {
+       'name': 'Micronase',
+       'type': 'brand',
+       'class_number': 2,
+       'currency': 'rouble',
+       'price_cents': 3900,
+       'unit_type': 'tablet',
+       'max_quantity': 30
+    }
+]
+
+
+def find_cheap_med(cnum, qty):
+    for med in medications:
+        if med['class_number'] == cnum:
+            fnd = []
+            fnd.append(med)
+    lp = 0
+    res = None
+    for med in fnd:
+        if qty * get_dollar_price(med) >= lp:
+            lp = qty * get_dollar_price(med)
+            res = med
+    if res['max_quantity'] > qty:
+        return 0
+    return get_dollar_price(res)
+    
+
+def get_dollar_price(med):
+    if med['currency'] == 'dollar':
+        return med['price_cents']
+    elif med['currency'] == 'euro':
+        return med['price_cents'] / 0.84 # 0.84 euro = 1 dollar
+    elif med['currency'] == 'rouble':
+        return med['price_cents'] / 73 # 73 rouble = 1 dollar
+    elif med['currency'] == 'pound':
+        return med['price_cents'] / 0.72 # 0.72 lb = 1 dollar
+    else:
+        return med['price_cents']
+    
+print(find_cheap_med(2, 15))
+
